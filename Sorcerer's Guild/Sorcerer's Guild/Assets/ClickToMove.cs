13ae7d9b13805794d213fa9717da39e2b7766753
Calum <CalumMcManus7@gmail.com>
2016-01-30 09:32:23 +0000

    + Adding all content for first push + Unity Project and Setting Files

---
+++ Sorcerer's Guild/Sorcerer's Guild/Assets/ClickToMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (NavMeshAgent))]
+public class ClickToMove : MonoBehaviour
+{
+    private string m_sGroundTag = "Ground";
+
+
+    private NavMeshAgent m_Agent;
+
+    private void Awake()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.tag == m_sGroundTag)
+                {
+                    m_Agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+
+}
