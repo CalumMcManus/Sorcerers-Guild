cef38ebba03f9146bd51dec5c06d48e07f1810d6
Calum <CalumMcManus7@gmail.com>
2016-01-30 15:36:56 +0000

    + Wwise integration and combat system start

---
+++ Sorcerer's Guild/Sorcerer's Guild/Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] private Transform m_Target;
+    public Transform Target { set { m_Target = value; } }
+    private Rigidbody m_Body;
+    private int m_iSpeed = 15;
+    public int Speed { set { m_iSpeed = value; } }
+
+    void OnEnable()
+    {
+        m_Body = GetComponent<Rigidbody>();
+        StartCoroutine(Follow());
+    }
+
+    private IEnumerator Follow()
+    {
+        while (Vector3.Distance(transform.position, m_Target.position) > 1f)
+        {
+            Vector3 vDir = (m_Target.position - transform.position).normalized;
+            m_Body.velocity = vDir*m_iSpeed;
+            yield return new WaitForEndOfFrame();
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Sorcerer's Guild/Sorcerer's Guild/Assets/Scripts/Combat/SingleTarget.cs
+++ Sorcerer's Guild/Sorcerer's Guild/Assets/Scripts/Combat/SingleTarget.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.InteropServices;
 
 public class SingleTarget : Ability
 {
@@ -7,12 +8,18 @@ public class SingleTarget : Ability
     private int m_iDamage;
     private int m_iDoT;
     private int m_iSelfHeal;
+    private int m_iCastTime;
+    private int m_iCoolDown;
+    private int m_iSlowPercent;
 
-    public void ContructorSingleTarget(DamageType dType, int damage, int dot, int heal)
+    public void ContructorSingleTarget(DamageType dType, int damage, int dot, int heal, int casttime, int cooldown, int slowPercent)
     {
         m_DamageType = dType;
         m_iDamage = damage;
         m_iDoT = dot;
         m_iSelfHeal = heal;
+        m_iCastTime = casttime;
+        m_iCoolDown = cooldown;
+        m_iSlowPercent = slowPercent;
     }
 }
